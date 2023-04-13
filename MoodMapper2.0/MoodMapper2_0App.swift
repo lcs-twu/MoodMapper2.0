5dc25223961ea91c4a27fec8038ef424a52f648b
Tom <twu@lcs.on.ca>
2023-04-13 11:08:37 -0400

    Initial Commit

---
+++ MoodMapper2.0/MoodMapper2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  MoodMapper2_0App.swift
+//  MoodMapper2.0
+//
+//  Created by Tom Wu on 2023-04-13.
+//
+
+import SwiftUI
+
+@main
+struct MoodMapper2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
